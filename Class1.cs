9b412509013692107a99f1cb2ae6f2123119de9d
Maxim Revnivtsev <max_rev1380@mail.ru>
2021-09-30 10:45:56 +0300

    init

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryPassword
+{
+    namespace ClassLibraryPassword
+    {
+        public class PasswordChecker
+        {
+            public static bool validatePassword(string password)
+            {
+                return true;
+            }
+        }
+    }
+}
